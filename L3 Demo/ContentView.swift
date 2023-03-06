e145ab466a6d8ed5f755d55d5b9c1ab1c6d269aa
Philip Keller <philipjoelkeller@gmail.com>
2023-03-06 14:41:25 -0800

    User Interface - Youtube 3/8 https://youtu.be/PCvopT3Wnek

--- L3 Demo/ContentView.swift
+++ L3 Demo/ContentView.swift
@@ -9,13 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+       
+        ZStack {
+            
+            Color(.systemMint)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20)  {
+                Image("niagrafalls")
+                    .resizable()
+                .scaledToFit()
+                .cornerRadius(8)
+                
+                
+                HStack {
+                    Text("Niagra Falls")
+                        .font(.title)
+                    .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        HStack {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                            
+                        }
+                        Text("(Reviews 361")
+                    }
+                    .foregroundColor(.orange)
+                    .font(.caption)
+                    
+                }
+                
+                Text("Come visit for the experience of a lifetime")
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "fork.knife")
+                    Image(systemName: "binoculars.fill")
+                }
+                .foregroundColor(.gray)
+                .font(.caption)
+            }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(16)
+                .shadow(radius: 15))
         .padding()
+        }
     }
 }
 
